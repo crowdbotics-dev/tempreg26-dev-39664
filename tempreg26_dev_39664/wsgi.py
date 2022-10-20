abfd7eaeb12ded56b0e6815159b212e44eb11e5f
Crowdbotics <engineering@crowdbotics.com>
2022-10-20 13:22:34 +0000

    Update models

--- tempreg26_dev_39664/wsgi.py
+++ tempreg26_dev_39664/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tempreg26_dev_39664.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tempreg26_dev_39664.settings")
 
 application = get_wsgi_application()
